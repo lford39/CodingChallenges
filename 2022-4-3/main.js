cf675b9cf4ca056548d659aea0909cbaadce281b
Lauren Ford <fordlauren39@gmail.com>
2022-04-15 13:23:07 -0400

    2022-4-3 Solution

---
+++ 2022-4-3/main.js
@@ -0,0 +1,6 @@
+//Implement a function which convert the given boolean value into its string representation.
+//Solution
+function booleanToString(b){
+    //b is the parameter, the method .toString() returns true or false for Boolean objects and values depending on value of the object
+    return b.toString();
+  }
