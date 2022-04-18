6e583e90123542d0bc952f246cc7ce53c122eebf
Lauren Ford <fordlauren39@gmail.com>
2022-04-18 12:27:04 -0400

    2022-04-18 Solution

---
+++ 2022-04-18/main.js
@@ -0,0 +1,8 @@
+//Simple, remove the spaces from the string, then return the resultant string.
+
+//Solution
+function noSpace(x){
+    let newString = x.split(' ').join('')
+    return newString
+    
+}
