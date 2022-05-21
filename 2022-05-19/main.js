ec2aa79d7bf717e4d8a9a75e9e8ce80b84d5db1c
Lauren Ford <fordlauren39@gmail.com>
2022-05-21 11:33:54 -0400

    @022-05-19 Solution

--- 2022-05-19/main.js
+++ 2022-05-19/main.js
@@ -18,7 +18,7 @@ function betterThanAverage(classPoints, yourPoints) {
 
 betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)
 
-//or
+  //or try
 // function betterThanAverage(classPoints, yourPoints) {
 //     return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
 //   }
